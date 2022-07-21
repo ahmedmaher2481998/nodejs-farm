9e257a14e4ed421cb88ffb1006984fd7cb1a6026
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2022-07-21 19:03:55 +0200

    added nodemon

--- index.js
+++ index.js
@@ -1,72 +1,64 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+
 const generateTemplate = require("./modules/generateTemplate");
 ////////////////////////
 //         Server
 // templates used
 const cardTemp = fs.readFileSync(`${__dirname}/templates/Card.html`, "utf-8");
-const overViewTemp = fs.readFileSync(
-	`${__dirname}/templates/overview.html`,
-	"utf-8"
-);
-const productTemp = fs.readFileSync(
-	`${__dirname}/templates/product.html`,
-	"utf-8"
-);
+const overViewTemp = fs.readFileSync(`${__dirname}/templates/overview.html`, "utf-8");
+const productTemp = fs.readFileSync(`${__dirname}/templates/product.html`, "utf-8");
 //data used
-const data = JSON.parse(
-	fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8")
-);
+const data = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8"));
+
 //generating template from html and product
 
 const server = http.createServer((req, res) => {
-	const path = req.url;
-	const parsedUrl = url.parse(req.url, true);
-	console.log(parsedUrl);
-	//product by id
-	if (parsedUrl.query.id && parsedUrl.pathname === "/product") {
-		const id = +parsedUrl.query.id;
-		const product = data.filter((el) => el.id === id)[0];
+    const path = req.url;
+    const parsedUrl = url.parse(req.url, true);
 
-		res.end(generateTemplate(productTemp, product));
-	}
-	//Home / overView page
-	else if (path === "/" || path === "/overview") {
-		console.log("generating data...");
+    //product by id
+    if (parsedUrl.query.id && parsedUrl.pathname === "/product") {
+        const id = +parsedUrl.query.id;
+        const product = data.filter((el) => el.id === id)[0];
 
-		res.writeHead(200, {
-			"Content-Type": "text/html",
-		});
+        res.end(generateTemplate(productTemp, product));
+    }
+    //Home / overView page
+    else if (path === "/" || path === "/overview") {
+        console.log("generating data...");
 
-		const cards = data.map((product) =>
-			generateTemplate(cardTemp, product)
-		);
-		const newOverview = overViewTemp.replace(/{%CARDS%}/g, cards.join());
-		res.end(newOverview);
-	}
-	// Product Page
-	else if (path === "/products") {
-		res.end("Hello from the products page !!");
-	}
-	// APi Route
-	else if (req.url === "/api") {
-		res.writeHead(200, {
-			"Content-Type": "application/json",
-		});
-		res.end(data);
-	}
-	//Not Found Template
-	else {
-		res.writeHead(404, {
-			"Content-Type": "text/html",
-		});
-		let p404 = fs.readFileSync(__dirname + "/templates/404.html", "utf-8");
-		p404 = p404.replace(/{%R%}/, path.slice(1));
+        res.writeHead(200, {
+            "Content-Type": "text/html",
+        });
 
-		res.end(p404);
-	}
+        const cards = data.map((product) => generateTemplate(cardTemp, product));
+        const newOverview = overViewTemp.replace(/{%CARDS%}/g, cards.join());
+        res.end(newOverview);
+    }
+    // Product Page
+    else if (path === "/products") {
+        res.end("Hello from the products page !!");
+    }
+    // APi Route
+    else if (req.url === "/api") {
+        res.writeHead(200, {
+            "Content-Type": "application/json",
+        });
+        res.end(data);
+    }
+    //Not Found Template
+    else {
+        res.writeHead(404, {
+            "Content-Type": "text/html",
+        });
+        let p404 = fs.readFileSync(__dirname + "/templates/404.html", "utf-8");
+        p404 = p404.replace(/{%R%}/, path.slice(1));
+
+        res.end(p404);
+    }
 });
 server.listen(8000, "127.0.0.1", () => {
-	console.log("Running on http://localhost:8000/");
+    console.log("Running on http://localhost:8000/");
 });
--- modules/generateTemplate.js
+++ modules/generateTemplate.js
@@ -1,14 +1,13 @@
 module.exports = (temp, product) => {
-	let output = temp.replace(/{%IMAGE%}/g, product.image);
-	output = output.replace(/{%PRODUCT_NAME%}/g, product.productName);
+    let output = temp.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRODUCT_NAME%}/g, product.productName);
 
-	output = output.replace(/{%QUN%}/g, product.quantity);
-	output = output.replace(/{%FROM%}/g, product.from);
-	output = output.replace(/{%PRICE%}/g, product.price);
-	if (!product.organic)
-		output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-	output = output.replace(/{%NEUTRINOS%}/g, product.nutrients);
-	output = output.replace(/{%DESCRIPTION%}/g, product.description);
-	output = output.replace(/{%ID%}/g, product.id);
-	return output;
+    output = output.replace(/{%QUN%}/g, product.quantity);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+    output = output.replace(/{%NEUTRINOS%}/g, product.nutrients);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+    return output;
 };
